a2abce636b078053db63e61f55a7d1974dcf14c2
yulanf <wxy717141894@outlook.com>
2019-03-19 11:40:54 +0800

    update file upload

--- src/main/java/com/suptnet/website/controller/RestFileUploadController.java
+++ src/main/java/com/suptnet/website/controller/RestFileUploadController.java
@@ -1,29 +1,29 @@
 package com.suptnet.website.controller;
 
 
+import com.suptnet.website.entity.FileInfo;
+import com.suptnet.website.mapper.FileInfoMapper;
 import com.suptnet.website.storage.StorageFileNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 public class RestFileUploadController {
@@ -32,36 +32,36 @@ public class RestFileUploadController {
     private String upload_folder;
     private static final Logger logger = LoggerFactory.getLogger(RestFileUploadController.class);
 
-    @GetMapping("/")
-    public List<Task> listUploadedFiles() throws IOException {
+    @Autowired(required = false)
+    private FileInfoMapper fileInfoMapper;
 
-        List<User> r = new ArrayList<User>(users.values());
-        return null;
+    @GetMapping("/")
+    public List<FileInfo> listUploadedFiles() throws IOException {
+
+        List<FileInfo> r = fileInfoMapper.findAll();
+        return r;
     }
 
 
 
     @PostMapping("/upload")
     public String singleFileUpload(MultipartFile file) {
-        if(Objects.isNull(file) || file.isEmpty()) {
-            return "文件为空，请重新上传";
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
         }
 
+        String fileName = file.getOriginalFilename();
+        String filePath = upload_folder;
+        File dest = new File(filePath + fileName);
+        logger.info(filePath + fileName);
         try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(upload_folder + file.getOriginalFilename());
-
-            if (!Files.isWritable(path))
-                Files.createDirectories(Paths.get(upload_folder));
-
-            Files.write(path, bytes);
-            logger.debug("文件写入成功");
-            return "文件上传成功";
-
-        } catch(IOException e) {
-            e.printStackTrace();
-            return "文件上传失败";
+            file.transferTo(dest);
+            logger.info("上传成功");
+            return "上传成功";
+        } catch (IOException e) {
+            logger.error(e.toString(), e);
         }
+        return "上传失败！";
     }
 
     @GetMapping("/download/{filename}")
@@ -77,7 +77,6 @@ public class RestFileUploadController {
             else {
                 throw new StorageFileNotFoundException(
                         "Could not read file: " + filename);
-
             }
         }
         catch (MalformedURLException e) {
